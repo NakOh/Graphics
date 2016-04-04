afcaa3616ec1f64f88060b614547e97c4692e8c2
NakOh <kk0704@naver.com>
2016-04-04 20:47:14 +0900

    저장용 푸쉬

--- Graphics01/Graphics01/MyCode.cpp
+++ Graphics01/Graphics01/MyCode.cpp
@@ -9,13 +9,14 @@ GLfloat g_x = 0.f;
 int g_timeDelta = 0, g_prevTime = 0;
 GLfloat g_velocityX = 5.f;
 GLfloat *vertices;
-GLubyte *indices;
+GLint *indices;
 
 void changeSize(GLsizei w, GLsizei h);
 void SetupRC(void);
 int readData();
 void RenderScene();
 void Idle();
+void parsingFloatData(string line,int count, int *vertexCount, int *indiceCount);
 
 int	main(int argc, char* argv[])
 {
@@ -33,12 +34,17 @@ int	main(int argc, char* argv[])
 }
 
 int readData() {
-
 	string line;
-	int index;
-	int info;
+	string temp;
 
-	ifstream myfile("models/Chair.dat");
+	int index;
+	int secondIndex;
+	int info;
+	int count = -1;
+	int vertexCount = 0;
+	int indiceCount = 0;
+
+	ifstream myfile("models/cube.dat");
 
 	if (myfile.is_open())
 	{			
@@ -48,12 +54,17 @@ int readData() {
 			if (line.find("VERTEX") != string::npos) { //만약 VERTEX가 있다면
 				index = line.find("="); //일단 = 를 찾는다.				
 				info = std::stoi(line.substr(index + 2, 4)); //찾은 인덱스부터 2번째 뒤부터 맨 끝까지의 string값을 int로 바꾼다.				
-				vertices = new GLfloat(info); // 해당 int만큼 배열 동적 생성
+				vertices = new GLfloat[info*3]; // 해당 int만큼 배열 동적 생성
+				count = 0;
 			} 
 			else if (line.find("FACE") != string::npos) { //만약 FACE가 있다면
 				index = line.find("="); //일단 = 를 찾는다.				
 				info = std::stoi(line.substr(index + 2, 4)); //찾은 인덱스부터 2번째 뒤부터 맨 끝까지의 string값을 int로 바꾼다.				
-				indices = new GLubyte(info); // 해당 int만큼 배열 동적 생성
+				indices = new GLint[info*3]; // 해당 int만큼 배열 동적 생성
+				count = 1;
+			}
+			else { //그냥 숫자 일때				
+				parsingFloatData(line, count, &vertexCount, &indiceCount);				
 			}
 		}
 		myfile.close();
@@ -63,6 +74,34 @@ int readData() {
 
 	return 0;
 
+}
+
+void parsingFloatData(string line,int count, int *vertexCount, int *indiceCount) {
+	int index;	
+	string temp;
+
+	if (count == 0) {
+		//VERTEX를 만난 후
+		index = line.find(" "); //띄어쓰기를 찾는다. 
+		temp = line.substr(0, index); //처음부터 그 인덱스까지 한 숫자
+		vertices[*vertexCount] = std::stof(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.
+		(*vertexCount)++;
+		temp = line.substr(index, 100);//띄어쓰기 이후로 단어를 임시 저장
+	}
+	else if (count == 1) {
+		//FACE를 만난 후
+		index = line.find(" "); //띄어쓰기를 찾는다. 
+		temp = line.substr(0, index); //처음부터 그 인덱스까지 한 숫자
+		vertices[*indiceCount] = std::stoi(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.
+		(*indiceCount)++;
+		temp = line.substr(index, 100);//띄어쓰기 이후로 단어를 임시 저장
+	}
+	else {
+		//VERTEX나 FACE를 못 만났을때
+		cout << "파일이 잘못되었습니다";
+		
+	}
+	
 }
 
 void Idle() {
