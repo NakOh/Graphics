7a18a7be727a06142d467dec50f00046a9ebe56f
NakOh <kk0704@naver.com>
2016-04-07 02:34:30 +0900

    과제끝!

--- Graphics01/Graphics01/MyCode.cpp
+++ Graphics01/Graphics01/MyCode.cpp
@@ -9,14 +9,21 @@ GLfloat g_x = 0.f;
 int g_timeDelta = 0, g_prevTime = 0;
 GLfloat g_velocityX = 5.f;
 GLfloat *vertices;
+
+GLfloat centerX;
+GLfloat centerY;
+GLfloat centerZ;
+
 GLint *indices;
 int info;
+int vertex;
 
 void changeSize(GLsizei w, GLsizei h);
 void SetupRC(void);
 int readData();
 void RenderScene();
 void Idle();
+void update();
 void parsingData(string line,int count, int *vertexCount, int *indiceCount);
 
 int	main(int argc, char* argv[])
@@ -27,7 +34,7 @@ int	main(int argc, char* argv[])
 	// callback function
 	glutDisplayFunc(RenderScene);
 	glutReshapeFunc(changeSize);
-	//glutIdleFunc(Idle);
+	glutIdleFunc(Idle);
 	SetupRC();		// initialize render context(RC)
 	glutMainLoop();		// run GLUT framework
 	return 0;	
@@ -38,13 +45,12 @@ int readData() {
 	string line;
 	string temp;
 
-	int index;
-	int secondIndex;
+	int index;	
 	int count = -1;
 	int vertexCount = 0;
 	int indiceCount = 0;
 
-	ifstream myfile("models/teapot.dat");
+	ifstream myfile("models/cube.dat");
 
 	if (myfile.is_open())
 	{			
@@ -53,8 +59,8 @@ int readData() {
 			//cout << line << '\n';
 			if (line.find("VERTEX") != string::npos) { //만약 VERTEX가 있다면
 				index = line.find("="); //일단 = 를 찾는다.				
-				info = std::stoi(line.substr(index + 2, 4)); //찾은 인덱스부터 2번째 뒤부터 맨 끝까지의 string값을 int로 바꾼다.				
-				vertices = new GLfloat[info*3]; // 해당 int만큼 배열 동적 생성
+				vertex = std::stoi(line.substr(index + 2, 4)); //찾은 인덱스부터 2번째 뒤부터 맨 끝까지의 string값을 int로 바꾼다.				
+				vertices = new GLfloat[vertex*3]; // 해당 int만큼 배열 동적 생성
 				count = 0;
 			} 
 			else if (line.find("FACE") != string::npos) { //만약 FACE가 있다면
@@ -85,19 +91,22 @@ void parsingData(string line,int count, int *vertexCount, int *indiceCount) {
 		//VERTEX를 만난 후
 		index = line.find(" "); //띄어쓰기를 찾는다. 
 		temp = line.substr(0, index); //처음부터 그 인덱스까지 한 숫자
-		vertices[*vertexCount] = std::stof(temp) / 200;  //숫자 하나 찾고 vertices에 집어 넣는다.	
+		vertices[*vertexCount] = std::stof(temp)/400;  //숫자 하나 찾고 vertices에 집어 넣는다.	
+		centerX += stof(temp);
 		(*vertexCount)++; //한 줄의 첫번째 변수 주입 완료	
 
 		line = line.substr(index+1, 100);//띄어쓰기 이후로 단어를 저장
 		index = line.find(" ");
 		temp = line.substr(0, index);
-		vertices[*vertexCount] = std::stof(temp) / 200;  //숫자 하나 찾고 vertices에 집어 넣는다.		
+		vertices[*vertexCount] = std::stof(temp)/400;  //숫자 하나 찾고 vertices에 집어 넣는다.		
+		centerY += stof(temp);
 		(*vertexCount)++; //한 줄의 두 번째 변수 주입 완료
 		
 		line = line.substr(index, 100);
 
 		temp = line.substr(1, 100); //두 번째 변수 이후로 임시 저장
-		vertices[*vertexCount] = std::stof(temp) / 200;  //숫자 하나 찾고 vertices에 집어 넣는다.
+		vertices[*vertexCount] = std::stof(temp)/400;  //숫자 하나 찾고 vertices에 집어 넣는다.
+		centerZ += stof(temp);		
 		(*vertexCount)++; //한 줄의 두 번째 변수 주입 완료	
 		
 	}
@@ -105,23 +114,18 @@ void parsingData(string line,int count, int *vertexCount, int *indiceCount) {
 		//FACE를 만난 후
 		index = line.find(" "); //띄어쓰기를 찾는다. 
 		temp = line.substr(0, index); //처음부터 그 인덱스까지 한 숫자
-		indices[*indiceCount] = std::stoi(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.
-		cout << indices[*indiceCount];
+		indices[*indiceCount] = std::stoi(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.		
 		(*indiceCount)++; //한 줄의 첫번째 변수 주입 완료		
 		
 		line = line.substr(index + 1, 100);//띄어쓰기 이후로 단어를 저장
 		index = line.find(" ");
 		temp = line.substr(0, index);
-		indices[*indiceCount] = std::stoi(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.		
-		cout << indices[*indiceCount];
+		indices[*indiceCount] = std::stoi(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.				
 		(*indiceCount)++; //한 줄의 두 번째 변수 주입 완료
 
 		line = line.substr(index, 100);
-		
-
 		temp = line.substr(1, 3); //두 번째 변수 이후로 임시 저장
-		indices[*indiceCount] = std::stoi(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.				
-		cout << indices[*indiceCount] << endl;
+		indices[*indiceCount] = std::stoi(temp);  //숫자 하나 찾고 vertices에 집어 넣는다.						
 		(*indiceCount)++; //한 줄의 세 번째 변수 주입 완료
 		
 	}
@@ -134,12 +138,7 @@ void parsingData(string line,int count, int *vertexCount, int *indiceCount) {
 
 
 void Idle() {
-	/*
-	int currentTime = glutGet(GLUT_ELAPSED_TIME);
-	g_timeDelta = currentTime - g_prevTime;
-	g_prevTime = currentTime;
-	glutPostRedisplay();
-	*/
+	glutPostRedisplay();	
 }
 
 void SetupRC(void)
@@ -149,17 +148,60 @@ void SetupRC(void)
 }
 
 void RenderScene() {
+	static GLfloat rot = 1.0f;
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0f, 0.5f, 0.0f);
+	glColor3f(1.f, 1.f, 1.f);
 	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	glLoadIdentity();
-	glEnableClientState(GL_VERTEX_ARRAY);
 	glVertexPointer(3, GL_FLOAT, 0, vertices);
-	glDrawElements(GL_TRIANGLES, info * 3, GL_UNSIGNED_INT, indices);
-	glDisableClientState(GL_VERTEX_ARRAY);
+	glLoadIdentity();	
+	glPushMatrix();
+	glPushMatrix();
+	glPushMatrix();
+	update();		
+	glutSwapBuffers();		
+}
+
+void update() {
+	static GLfloat rot = 0.0f;
+	static GLfloat scale = 1.0f;
+	static GLfloat x = 0.0001f;
+	static GLfloat center_x = centerX / (vertex *  400);
+	static GLfloat center_y = centerY / (vertex * 400);
+	static GLfloat center_z = centerZ / (vertex * 400);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+
+	//자신 중심 회전
 	glPopMatrix();
-	glutSwapBuffers();
+	glTranslatef(-0.7f, 0, 0);
+	glTranslatef(center_x , center_y, center_z);
+	glRotatef(rot, 0.0, 0.0, 1.0);
+	glTranslatef(-(center_x) , -center_y, -center_z);	
+	glDrawElements(GL_TRIANGLES, info * 3, GL_UNSIGNED_INT, indices);
+	
+	rot -= 0.005f;	
+
+	//크기 커짐 작아짐	
+	glPopMatrix();
+	glTranslatef(0.5f, 0.5f, 0.f);
+	glScalef(scale, scale, scale);
+	if (scale > 1.5f){
+		x = -x;
+	}
+	else  if (scale < 0){
+		x = -x;
+	}	
+	scale += x;
+	glDrawElements(GL_TRIANGLES, info * 3, GL_UNSIGNED_INT, indices);	
+
+	//다른 점 중심 회전	
+	glPopMatrix();
+	glTranslatef(center_x , center_y - 0.5f, center_z);
+	glRotatef(rot, 0.0, 0.0, 1.0);
+	glTranslatef(-center_x, -(center_y - 0.5f) , -center_z);
+	glDrawElements(GL_TRIANGLES, info * 3, GL_UNSIGNED_INT, indices);	
+
+	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
 void changeSize(GLsizei w, GLsizei h) {
